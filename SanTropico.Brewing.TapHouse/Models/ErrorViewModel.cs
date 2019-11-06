85ed24aeac095b1ae08ada0f773884c5464a750c
bkinney91 <bkinney91@users.noreply.github.com>
2019-11-06 15:50:50 -0800

    Re-add template for ASP.NET CORE 3

---
+++ SanTropico.Brewing.TapHouse/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SanTropico.Brewing.TapHouse.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
