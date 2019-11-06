9d00c65f4bf692988105ed3d30c7c122a80e8766
bkinney91 <bkinney91@users.noreply.github.com>
2019-11-06 15:33:16 -0800

    Add ASP.NET Core 3.0 MVC template
    Also add .gitignore from dotnet repository

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SanTropico.Brewing.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
