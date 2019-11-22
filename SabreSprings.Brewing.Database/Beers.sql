016f6e990a9ce5bff0754d604c768b5c2d563db4
bkinney91 <bkinney91@users.noreply.github.com>
2019-11-21 21:15:21 -0800

    Convert first table to sqlite3

--- SabreSprings.Brewing.Database/Beers.sql
+++ SabreSprings.Brewing.Database/Beers.sql
@@ -1,9 +1,8 @@
-﻿CREATE TABLE [dbo].[Beers]
-(
-	[Id] INT IDENTITY NOT NULL PRIMARY KEY, 
-    [Name] NVARCHAR(55) NULL, 
-    [Style] NVARCHAR(255) NULL,
-	[SuggestedGlassType] nvarchar(255) null,
-	[Created] DateTime default GETDATE(),
-	[CreatedBy] Nvarchar(255) null
+﻿CREATE TABLE Beers (
+	Id INTEGER PRIMARY KEY, 
+    Name TEXT NULL, 
+    Style TEST NULL,
+	SuggestedGlassType TEXT null,
+	Created DateTime default CURRENT_TIMESTAMP,
+	CreatedBy TEXT null
 )
