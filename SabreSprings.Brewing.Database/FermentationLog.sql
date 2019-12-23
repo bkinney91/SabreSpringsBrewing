b0ce0925f8feac9d2dc57196be5bda85b0a703a2
bkinney91 <bkinney91@users.noreply.github.com>
2019-12-22 19:19:28 -0800

    Add fermentation log for Ispindel logging

---
+++ SabreSprings.Brewing.Database/FermentationLog.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[FermentationLog]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	[Batch] INTEGER NOT NULL,
+	[Temperature] DECIMAL NOT NULL,
+	[Gravity] DECIMAL NOT NULL,
+	[Angle] DECIMAL NOT NULL,
+	[DeviceNumber] INTEGER NOT NULL,
+	[Created] DateTime default CURRENT_TIMESTAMP,
+	FOREIGN KEY(Batch) REFERENCES Batches(Id),
+)
