b3e5d4b1572ef892aa23646e8d5da1c451381bdc
Brent Kinney <48770271+brentkinney@users.noreply.github.com>
2020-04-14 17:07:22 -0700

    Added chart files and displayed data through chart

---
+++ SabreSprings.Brewing.Web/wwwroot/js/fermentationChart.js
@@ -0,0 +1,75 @@
+﻿$(document).ready(function () {
+    GetAllLogs();
+});
+
+
+
+function GetAllLogs() {
+    $.ajax({
+        url: "/api/FermentabuoyLog/GetAll",
+        type: 'GET',
+        dataType: "json",
+        contentType: "application/json;charset=utf-8",
+        success: function (data) {
+            DisplayLogsChart(data);
+        },
+        error: function (data) {
+
+        }
+    });
+}
+
+function DisplayLogsChart(data) {
+    var fermentationChart = $("#fermentationChart").dxChart({        
+        dataSource: data,
+        commonSeriesSettings: {
+            argumentField: "created",
+            aggregationInterval: "week",
+            displayFormat: "short",
+            //valueField: "name",
+            type: "line"
+        },
+        margin: {
+            bottom: 20
+        },
+        size: {
+            height: 800,
+            width: 1000
+        },
+        argumentAxis: {
+            valueMarginsEnabled: false,
+            discreteAxisDivisionMode: "crossLabels",
+            tickInterval: { weeks: 2 },
+            grid: {
+                visible: true
+            }
+        },
+        series: [            
+            { valueField: "temperature", name: "Temperature" },
+            { valueField: "gravity", name: "Gravity" },
+            { valueField: "angle", name: "Angle" },
+            { valueField: "battery", name: "Battery" },
+            { valueField: "rssi", name: "RSSI" }
+        ],
+        legend: {
+            verticalAlignment: "bottom",
+            horizontalAlignment: "center",
+            itemTextPosition: "bottom"
+        },
+        title: {
+            text: "Fermentation Chart",
+            subtitle: {
+                text: "(Timelapse)"
+            }
+        },
+        "export": {
+            enabled: true
+        },
+        tooltip: {
+            enabled: true
+        },
+        loadingIndicator: {
+            enabled: true
+        }
+     }).dxChart("instance"); 
+}
