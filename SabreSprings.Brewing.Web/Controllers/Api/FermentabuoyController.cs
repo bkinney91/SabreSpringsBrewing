5cd4b467eeefd75ae2a378d7f70404af956e27ca
bkinney91 <bkinney91@users.noreply.github.com>
2020-08-07 11:10:39 -0700

    Add Get, Post, and Put

--- SabreSprings.Brewing.Web/Controllers/Api/FermentabuoyController.cs
+++ SabreSprings.Brewing.Web/Controllers/Api/FermentabuoyController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SabreSprings.Brewing.Models.DataTransfer;
+using SabreSprings.Brewing.Models.Entities;
 using SabreSprings.Brewing.Services;
 using SabreSprings.Brewing.Services.Interfaces;
 
@@ -35,6 +36,58 @@ namespace SabreSprings.Brewing.Web.Controllers.Api
                 return StatusCode(StatusCodes.Status500InternalServerError, ex);
             }
         }
+
+
+
+        [HttpGet]
+        [Route("GetFermentabuoy")]
+        public async Task<IActionResult> GetFermentabuoy(int id)
+        {
+            try
+            {
+                FermentabuoyDto fermentabouy = await FermentabuoyService.GetFermentabuoy(id);
+                return Ok(fermentabouy);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex);
+            }
+        }
+
+
+        [HttpPost]
+        [Route("AddFermentabuoy")]
+        public async Task<IActionResult> AddFermentabuoy(FermentabuoyDto dto)
+        {
+            try
+            {
+                await FermentabuoyService.AddFermentabuoy(dto);
+                return NoContent();
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex);
+            }
+        }
+
+
+        [HttpPut]
+        [Route("AddFermentabuoy")]
+        public async Task<IActionResult> UpdateFermentabuoy(FermentabuoyDto dto)
+        {
+            try
+            {
+                await FermentabuoyService.UpdateFermentabuoy(dto);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex);
+            }
+        }
+
+
+
 
     }
 }
