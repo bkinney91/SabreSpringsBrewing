f396bd6a72770d05b9210cc344faa1cd9147bc83
Bryce Kinney <brycekinney91@gmail.com>
2020-11-12 18:17:38 -0800

    Start adding beer service

--- SabreSprings.Brewing.Data/Interfaces/IBeerDataProvider.cs
+++ SabreSprings.Brewing.Data/Interfaces/IBeerDataProvider.cs
@@ -1,11 +1,14 @@
 ﻿using SabreSprings.Brewing.Models.Entities;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace SabreSprings.Brewing.Data.Interfaces
 {
     public interface IBeerDataProvider
     {
         Task<Beer> GetBeer(int id);
-        
+        Task Add(Beer beer);
+        Task Update(Beer beer);
+        Task<List<Beer>> GetBeers();
     }
 }
---
+++ SabreSprings.Brewing.Services/Interfaces/IBeerService.cs
@@ -0,0 +1,16 @@
+using SabreSprings.Brewing.Models.DataTransfer;
+using SabreSprings.Brewing.Models.Entities;
+using SabreSprings.Brewing.Models.View;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SabreSprings.Brewing.Services.Interfaces
+{
+    public interface IBeerService
+    {
+        Task<List<Beer>> GetBeers();
+        Task<Beer> GetBeer(int id);
+         Task Update(BeerDto dto);
+         Task Add(BeerTo dto);        
+    }
+}
