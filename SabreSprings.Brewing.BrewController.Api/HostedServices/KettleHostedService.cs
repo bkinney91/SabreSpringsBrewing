ad4d75a4249be3466a73aae6ba577261db614e71
bkinney91 <bkinney91@users.noreply.github.com>
2020-12-03 19:23:15 -0800

    change interval to every 5

--- SabreSprings.Brewing.BrewController.Api/HostedServices/KettleHostedService.cs
+++ SabreSprings.Brewing.BrewController.Api/HostedServices/KettleHostedService.cs
@@ -26,7 +26,7 @@ namespace SabreSprings.Brewing.BrewController.HostedServices
         public Task StartAsync(CancellationToken stoppingToken)
         {
             _timer = new Timer(DoWork, null, TimeSpan.Zero,
-                TimeSpan.FromSeconds(10));
+                TimeSpan.FromSeconds(5));
             return Task.CompletedTask;
         }
 
