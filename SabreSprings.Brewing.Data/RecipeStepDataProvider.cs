cb6dfa0ebacd6cfe67b9bc56ed2884be0d8119ea
bkinney91 <bkinney91@users.noreply.github.com>
2020-12-24 09:03:38 -0800

    Add some classes

---
+++ SabreSprings.Brewing.Data/RecipeStepDataProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SabreSprings.Brewing.Data
+{
+    public class RecipeStepDataProvider
+    {
+    }
+}
---
+++ SabreSprings.Brewing.Models/Entities/RecipeStep.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SabreSprings.Brewing.Models.Entities
+{
+    public class RecipeStep
+    {
+        public int Id { get; set; }
+        public int Recipe { get; set; }
+        public string Stage { get; set; }
+        public int StepNumber { get; set; }
+        public string DisplayText { get; set; }
+        public int? TimerMinutes { get; set; }        
+    }
+}
