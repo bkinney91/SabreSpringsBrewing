c7770c3857b1a08300fe72b4997b92b9ce3b7f53
Bryce Kinney <brycekinney91@gmail.com>
2020-12-29 20:43:28 -0800

    Add brew controller api script content

--- InstallScripts/InstallBrewControllerApi.sh
+++ InstallScripts/InstallBrewControllerApi.sh
@@ -1,2 +1,14 @@
 #!/bin/sh
 # Install SSBE Brew Controller Api
+echo "Cleaning API workspace"
+cd BrewControllerApi
+sudo rm * -r -f
+echo "Clone source code"
+git clone https://github.com/bkinney91/SabreSpringsBrewing.git 
+cd SabreSpringsBrewing/SabreSprings.Brewing.BrewController.Api
+echo "Building project"
+sudo /usr/share/dotnetsdk/dotnet publish -c Production
+sudo cp bin/Production/net5.0/publish/* /home/bryce/BrewControllerApi -r 
+echo "Cleaning up"
+cd /home/bryce
+sudo rm BrewControllerApi/SabreSpringsBrewing -r -f
--- InstallScripts/InstallBrewControllerClient.sh
+++ InstallScripts/InstallBrewControllerClient.sh
@@ -1,2 +1,16 @@
 #!/bin/sh
 # Install SSBE Brew Controller Client
+
+cd BrewControllerClient
+sudo rm * -r -f
+echo "Clone source code"
+git clone https://github.com/bkinney91/SabreSpringsBrewing.git 
+cd SabreSpringsBrewing/SabreSprings.Brewing.BrewController.Client
+echo "NPM Install"
+npm install
+echo "Building project"
+npm run build
+sudo cp dist /var/www/html -r 
+echo "Cleaning up"
+cd /home/bryce
+#sudo rm BrewControllerClient/SabreSpringsBrewing -r -f
