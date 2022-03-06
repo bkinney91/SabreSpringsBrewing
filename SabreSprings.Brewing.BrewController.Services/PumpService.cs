f845879cdba9a57a10a6e16b80694c7b5964da79
Bryce Kinney <brycekinney91@gmail.com>
2022-03-06 12:01:13 -0800

    Add protections for calling rpi functions on dev

--- SabreSprings.Brewing.BrewController.Services/PumpService.cs
+++ SabreSprings.Brewing.BrewController.Services/PumpService.cs
@@ -15,10 +15,10 @@ namespace SabreSprings.Brewing.BrewController.Services
         public PumpService()
         {
             Pump1Enabled = false;
-            Pump2Enabled = false;
-            Rpi = new GpioController();
+            Pump2Enabled = false;           
             if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == Environments.Production)
             {
+                Rpi = new GpioController();
                 Rpi.OpenPin(Pump1Pin, PinMode.Output);
                 Rpi.OpenPin(Pump2Pin, PinMode.Output);
             }
@@ -32,24 +32,46 @@ namespace SabreSprings.Brewing.BrewController.Services
         public void Pump1(bool enablePower)
         {
             Pump1Enabled = enablePower;
-            Rpi.Write(Pump1Pin, enablePower ? PinValue.High : PinValue.Low);
+            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == Environments.Production)
+            {
+                Rpi.Write(Pump1Pin, enablePower ? PinValue.High : PinValue.Low);
+            }
         }
 
         public void Pump2(bool enablePower)
         {
             Pump2Enabled = enablePower;
-            Rpi.Write(Pump2Pin, enablePower ? PinValue.High : PinValue.Low);
+            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == Environments.Production)
+            {
+                Rpi.Write(Pump2Pin, enablePower ? PinValue.High : PinValue.Low);
+            }
         }
 
         public bool GetPump1PowerState()
         {
-            PinValue pinValue = Rpi.Read(Pump1Pin);
+            PinValue pinValue;
+            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == Environments.Production)
+            {
+                pinValue = Rpi.Read(Pump1Pin);
+            }
+            else
+            {
+                pinValue = Pump1Enabled ? PinValue.High: PinValue.Low;
+            }
             return pinValue == PinValue.High;
         }
 
         public bool GetPump2PowerState()
         {
-            PinValue pinValue = Rpi.Read(Pump2Pin);
+            PinValue pinValue;
+            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == Environments.Production)
+            {
+                pinValue = Rpi.Read(Pump2Pin);
+            }
+            else
+            {
+                pinValue = Pump1Enabled ? PinValue.High : PinValue.Low;
+            }
             return pinValue == PinValue.High;
         }
 
